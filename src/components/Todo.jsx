5f055e6f55185865fb32c8697daf740e2ed8df80
Fayshal007 <fayshalislam5@gmail.com>
2024-08-31 11:51:40 +0600

    input value put to  a new state

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,23 +1,48 @@
-import './Todo.css'
+import { useState } from "react";
+import "./Todo.css";
 const Todo = () => {
-    return (
-        <section className="todo-container">
-            <header>
-                <h1>Tode List</h1>
-            </header>
-            <section className='form'>
-                <form >
-                    <div>
-                        <input type="text" className='todo-input' autoComplete='off'/>
-                    </div>
-                    <div>
-                        <button type='submit' className="todo-btn">Add Task</button>
-                    </div>
-                </form>
-            </section>
-            
-        </section>
-    );
+  const [inputValue, setValue] = useState("");
+
+  const [task, setTask] = useState([])
+
+  const handleInputChange = (value) => {
+    setValue(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!inputValue) {
+        return;
+    }
+    setTask((prev) => [...prev, inputValue])
+  };
+
+  return (
+    <section className="todo-container">
+      <header>
+        <h1>Tode List</h1>
+      </header>
+      <section className="form">
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input
+              type="text"
+              className="todo-input"
+              autoComplete="off"
+              value={inputValue}
+              onChange={(e) => handleInputChange(e.target.value)}
+            />
+          </div>
+          <div>
+            <button type="submit" className="todo-btn">
+              Add Task
+            </button>
+          </div>
+        </form>
+      </section>
+    </section>
+  );
 };
 
 export default Todo;
